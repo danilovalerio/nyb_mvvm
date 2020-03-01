e209d23e5584670cb6c784e88a651057a2b20c56
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 20:16:44 -0300

    Organização do projeto com os pacotes separando as responsabilidades Renomear a activity principal e seu layout res

---
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package projetos.danilo.nybooksmvvm.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projetos.danilo.nybooksmvvm.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booksn)
+    }
+}
