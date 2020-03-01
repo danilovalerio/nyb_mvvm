390aa04542dd9d4a590ba382c2814a43c6385d5c
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 20:43:05 -0300

    BooksAdapter inicial

---
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksAdapter.kt
@@ -0,0 +1,23 @@
+package projetos.danilo.nybooksmvvm.presentation.books
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    class BooksViewHolder : RecyclerView.ViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
