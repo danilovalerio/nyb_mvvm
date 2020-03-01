2e2d2a85d7351efa136bafcca55ae6e1945b897d
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 20:32:11 -0300

    Adição da Toolbar e do RecyclerView

--- app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -2,6 +2,7 @@ package projetos.danilo.nybooksmvvm.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_booksn.*
 import projetos.danilo.nybooksmvvm.R
 
 class BooksActivity : AppCompatActivity() {
@@ -9,5 +10,8 @@ class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booksn)
+
+        toolbarPrincipal.title = "Livros"
+        setSupportActionBar(toolbarPrincipal)
     }
 }
