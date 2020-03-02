eeeedb3f5439f4beaad0f7168e58cd4b8bb857fa
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 21:19:27 -0300

    Refactor no nome activity_books.xlm

--- app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_booksn.*
+import kotlinx.android.synthetic.main.activity_books.*
 import projetos.danilo.nybooksmvvm.R
 import projetos.danilo.nybooksmvvm.data.model.Book
 
@@ -12,7 +12,7 @@ class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_booksn)
+        setContentView(R.layout.activity_books)
 
         toolbarPrincipal.title = getString(R.string.titulo_livros)
         setSupportActionBar(toolbarPrincipal)
