4b1d9a4a4fda8688ab7b2f3858f3fdfea57a4ee9
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 21:12:44 -0300

    Lista de livros para teste e adapter finalizado recebendo a lista "fake"

--- app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -2,8 +2,11 @@ package projetos.danilo.nybooksmvvm.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_booksn.*
 import projetos.danilo.nybooksmvvm.R
+import projetos.danilo.nybooksmvvm.data.model.Book
 
 class BooksActivity : AppCompatActivity() {
 
@@ -13,5 +16,20 @@ class BooksActivity : AppCompatActivity() {
 
         toolbarPrincipal.title = getString(R.string.titulo_livros)
         setSupportActionBar(toolbarPrincipal)
+
+        with(recyclerBooks) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)//para melhor performance
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    //lista para teste manual
+    fun getBooks(): List<Book> {
+        return listOf<Book>(
+            Book("Clean Code", "Robert C. Martin"),
+            Book("Programando com Kotlin", "Stephen Samuel"),
+            Book("Scrum e Agile em Projetos", "Fábio Cruz")
+        )
     }
 }
