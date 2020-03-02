f5beebc2c3d6a841ef7758bb1d1d33970cccfec6
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-01 21:05:36 -0300

    Model Book created Adapter finaliado

---
+++ app/src/main/java/projetos/danilo/nybooksmvvm/data/model/Book.kt
@@ -0,0 +1,6 @@
+package projetos.danilo.nybooksmvvm.data.model
+
+data class Book (
+    val titulo: String,
+    val autor: String
+)
--- app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksAdapter.kt
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksAdapter.kt
@@ -1,23 +1,35 @@
 package projetos.danilo.nybooksmvvm.presentation.books
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_book.view.*
+import projetos.danilo.nybooksmvvm.R
+import projetos.danilo.nybooksmvvm.data.model.Book
 
-class BooksAdapter : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
-
-    class BooksViewHolder : RecyclerView.ViewHolder(){
-
-    }
+class BooksAdapter(
+        val livros: List<Book>
+) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BooksViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount() = livros.count()
 
     override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindView(livros[position])
+    }
+
+    class BooksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titulo = itemView.textoTitulo
+        val autor = itemView.textoAutor
+
+        fun bindView(book: Book){
+            titulo.text = book.titulo
+            autor.text = book.autor
+        }
     }
 }
