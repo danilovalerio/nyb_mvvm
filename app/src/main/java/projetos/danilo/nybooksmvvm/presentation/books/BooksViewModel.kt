a36b814aa234d1e609df6feb76720f6a3124d712
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-07 21:50:34 -0300

    liveData para realizar a comunicação com a View

--- app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksViewModel.kt
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/books/BooksViewModel.kt
@@ -1,9 +1,25 @@
 package projetos.danilo.nybooksmvvm.presentation.books
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import projetos.danilo.nybooksmvvm.data.model.Book
 
 class BooksViewModel: ViewModel() {
     //não receber nenhuma referência da activity ou fragment aqui
 
-    
+    //LiveData é componente sugerido para fazer a comunicação com a View
+    val livrosLiveData: MutableLiveData<List<Book>> = MutableLiveData()
+
+    fun getBooks(){
+        livrosLiveData.value = createFakeBooks()
+    }
+
+    //lista para teste manual
+    fun createFakeBooks(): List<Book> {
+        return listOf<Book>(
+            Book("Clean Code", "Robert C. Martin"),
+            Book("Programando com Kotlin", "Stephen Samuel"),
+            Book("Scrum e Agile em Projetos", "Fábio Cruz")
+        )
+    }
 }
