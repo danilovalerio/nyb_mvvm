691e4db2f06fe87e161aa6f8687a1f1a1aaebfdd
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-15 20:37:13 -0300

    Aparência básica do books details

---
+++ app/src/main/java/projetos/danilo/nybooksmvvm/presentation/details/BooksDetailsActivity.kt
@@ -0,0 +1,13 @@
+package projetos.danilo.nybooksmvvm.presentation.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projetos.danilo.nybooksmvvm.R
+
+class BooksDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_details)
+    }
+}
